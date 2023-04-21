549f4bd9dd249213006948e5ec86f450673e9170
Bilal Malik <bilalmalik42011@gmail.com>
2023-04-22 01:12:22 +0500

    Chatbot Created

---
+++ bot.js
@@ -0,0 +1,68 @@
+import { Client } from "whatsapp-web.js";
+import qrCode from "qrcode-terminal";
+
+console.log("Inside Bot.js");
+
+const msg = "Eid Mubarak Bilal Bhai";
+if (msg.toLowerCase().includes("eid mubarak")) {
+  console.log("Yes included");
+}
+const client = new Client();
+
+client.on("qr", (qr) => {
+  console.log("Qr is: ", qr);
+  qrCode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready");
+});
+
+client.on("message", async (message) => {
+  try {
+    console.log(message);
+    const contact = await message.getContact();
+
+    if (message.body.toLowerCase().includes("eid mubarak")) {
+      console.log("Inside eid mubarak");
+      const senderName = contact?.name || contact?.pushname;
+      console.log(senderName);
+      message.reply(
+        `${senderName && senderName} Bhai Khair Mubarak❤ apko b Eid Mubarak ❤ `
+      );
+    }
+
+    // My Profil Url Chat
+    if (message.body.toLowerCase() == "my profile url") {
+      const profileUrl = await contact.getProfilePicUrl();
+      console.log(profileUrl);
+      client.sendMessage(
+        message.from,
+        `Your Profile Pic Url: ${
+          profileUrl ? profileUrl : "Privacy enabled not allowed"
+        }`
+      );
+    }
+
+    // Our Common Groups Chat
+    if (message.body.toLowerCase() == "our common groups") {
+      const groups = await contact.getCommonGroups();
+      let groupsString = "";
+      if (groups && groups?.length > 0) {
+        for (let i = 0; i < groups.length; i++) {
+          const chat = await client.getChatById(groups[i]._serialized);
+          console.log(chat?.name);
+          groupsString += `${i + 1}. ${chat?.name}.\n `;
+        }
+      }
+      client.sendMessage(
+        message.from,
+        `Groups in common: \n${groups ? groupsString : "No group common"}`
+      );
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+client.initialize();
